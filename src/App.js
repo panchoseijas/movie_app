3ddfadbc6539e26f129b0ef18d49c7766bfe382f
Francisco Seijas <panchoseijas@gmail.com>
2023-08-29 09:37:39 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import MovieConteiner from './components/Movie_Conteiner/Movie_conteiner'
-import Header from './components/Header/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 import MainMovies from './components/MainMovies/MainMovies';
 import MoviePage from './components/MoviePage/MoviePage';
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './Footer.css'
-
-
+import link_icon from '../LI-In-Bug.png'
+import gmail_icon from '../Gmail_Logo.png'
 
 const Footer = () => {
     return (
         <footer>
             <div className="contact-info">
                 <div className="info-details">
-                    <img src="Gmail_icon.png"></img>
+                    <img src={gmail_icon}></img>
                     <a href="mailto:seijasjuanfrancisco@gmial.com">seijasjuanfrancisco@gmail.com</a>
                 </div>
                 <div className="info-details">
-                    <img src='LI-In-Bug.png'></img>
+                    <img src={link_icon}></img>
                     <a href="https://www.linkedin.com/in/juan-francisco-seijas-97a427275">Juan Francisco Seijas</a>
                 </div>
             </div>
